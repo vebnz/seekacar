8df03a981472bb29e7cdd5f6cc77590358056647
Jamie Gracie <k1hosting@gmail.com>
2013-06-08 22:26:50 +1200

    updated index page

--- application/views/rental/index.php
+++ application/views/rental/index.php
@@ -1,69 +1,75 @@
-<script type='text/javascript' language='javascript'>
-$(document).ready(function() {
-	$('#submit').click(function(){ 
-		$('#result_table').empty();
-		$('#debug').empty();
-		$('#result_table').append("Fetching Cars...");
-		$.ajax({
-            url: 'rental/get_cars',
-            type:'POST',
-			dataType: 'json',
-			data: {pickuplocation: $('#pickuplocation').val(),	pickupdate: $('#pickupdate').val(),	pickuptime: $('#pickuptime').val(),	dropofflocation: $('#dropofflocation').val(), dropoffdate: $('#dropoffdate').val(),	dropofftime: $('#dropofftime').val()},
-            success: function(cars){
-					$('#result_table').empty();
-					$('#debug').empty();
-					
-					$.each(cars, function(i, item) {
-						$('#result_table').append(item.company + " - " + item.title + " - " + item.type + " - " + item.gearbox + " - " + item.size + " - " + item.price + "<br />"); 
-					});
-					
-                    $('#debug').append(cars);
-            } // End of success function of ajax form
-        }); // End of ajax call  
-	});
-});
-</script>
-	
-<h3>Welcome to Rental Cars</h3>
-Search for a Rental Car!
+ 		<div class="app_view">
+		<div class="hero">
+			<ul class="slideshow">
+				<li>
+					<img alt="Writer's Studio" src="/images/background.jpg" height="700" width="1600" style="min-width: 1600px; top: -100.3658536585366px;">
+				</li>
+			</ul>
+			<div class="search-form">
+				<div class="contained well">
+					<h2>Find Rentals</h2>
+					<?php echo validation_errors(); ?>
+                    <?php echo form_open('rental/index') ?>
 
-<?php echo validation_errors(); ?>
-<?php echo form_open('rental/index') ?>
+						<fieldset>
+							<div class="clearfix">
+                                <select id="pickuplocation" name="pickuplocation" class="input-xlarge">
+                                    <?php foreach ($locations as $location): ?>
+                                        <option value="<?php echo $location['city']; ?>"><?php echo  $location['city']; ?></option>
+                                    <?php endforeach ?>  
+                                </select>
+							</div>
+							<div class="clearfix">
+								<input class="input-medium" id="pickupdate"  name="pickupdate" type="text" placeholder="Pickup Date"> 
+                                <input class="input-small" id="pickuptime"  name="pickuptime" type="text" placeholder="Pickup Time">
+							</div>
+							<div class="clearfix">
+                                <select id="dropofflocation" name="dropofflocation" class="input-xlarge">
+                                    <?php foreach ($locations as $location): ?>
+                                        <option value="<?php echo $location['city']; ?>"><?php echo $location['city']; ?></option>
+                                    <?php endforeach ?> 
+                                </select>
+							</div>
+							<div class="clearfix">
+								<input class="input-medium" id="dropoffdate" name="dropoffdate" type="text" placeholder="Dropoff Date"> 
+                                <input class="input-small" id="dropofftime" name="dropofftime" type="text" placeholder="Dropoff Time">
+							</div>
+							<div class="clearfix">
+								<button class="btn btn-large btn-primary" id="submit" name="submit" type="submit">Search</button>
+							</div>
+						</fieldset>
+					</form>
+					</div>
+				</div>               
+            
+		</div>
+		</div>
+		<div class="car-brands pagination-centered">
+                    <img src="/images/ace.jpg" alt="Ace Rental Cars" /><img src="/images/apex.jpg" alt="Apex Rental Cars" /><img src="/images/britz.jpg" alt="Britz Rental Cars" /><img src="/images/budget.jpg" alt="Budget Rental Cars" /><img src="/images/omega.jpg" alt="Omega Rental Cars" /><img src="/images/thrifty.jpg" alt="Thrifty Rental Cars" />
+        </div>
+          <hr>
+		    <div class="main">
+            <div class="row-fluid boxes">
+            <div class="span4">
+              <h2>Search</h2>
+              <p>Search destinations, dates, and times.</p>
+              <p><a class="btn btn-success btn-large" href="#">Search</a></p>
+            </div><!--/span-->
+            <div class="span4">
+              <h2>Compare</h2>
+              <p>Compare results from multiple car rental companies.</p>
+              <p><a class="btn btn-success btn-large" href="#">Compare</a></p>
+            </div><!--/span-->
+            <div class="span4">
+              <h2>Rent!</h2>
+              <p>Choose the car you like and book it.</p>
+              <p><a class="btn btn-success btn-large" href="#">Rent</a></p>
+            </div><!--/span-->
+          </div><!--/row-->
+          <hr />
+            <h1>Pick 'a' Rental!</h1>
+            <p>Search, compare, and choose rental cars from multiple car rental agencies on specified dates and times of your choosing.</p>
 
-<h3>Pick-Up</h3>
-
-<label for="pickuplocation">Pick-Up Location:</label> 
-<select id="pickuplocation" name="pickuplocation">
-<?php foreach ($locations as $location): ?>
-<option value="<?php echo $location['city']; ?>"><?php echo  $location['city']; ?></option>
-<?php endforeach ?>    
-</select><br />
-
-<label for="pickupdate">Pick-Up Date:</label>
-<input id="pickupdate"  name="pickupdate" type="text" /><br />
-
-<label for="pickuptime">Pick-Up Time:</label> 
-<select id="pickuptime"  name="pickuptime"><option value="1">1:00</option><option value="2">2:00</option><option value="3">3:00</option></select><br />
-
-<h3>Drop-Off</h3>
-
-<label for="dropofflocation">Drop-Off Location:</label> 
-<select id="dropofflocation"  name="dropofflocation">
-<?php foreach ($locations as $location): ?>
-<option value="<?php echo $location['city']; ?>"><?php echo $location['city']; ?></option>
-<?php endforeach ?>    
-</select><br />
-
-<label for="dropoffdate">Drop-Off Date:</label>
-<input id="dropoffdate" name="dropoffdate" type="text" /><br />
-
-<label for="dropofftime">Drop-Off Time:</label> 
-<select id="dropofftime" name="dropofftime"><option value="1">1:00</option><option value="2">2:00</option><option value="3">3:00</option></select><br />
-
-<input type="button" id="submit" name="submit" value="Search Cars" /> 
-
-</form>
-
-<div id="result_table"></div><br /><br />    
-<div id="debug"></div>    
-<br /><br />
+          </div>
+          
+ 
