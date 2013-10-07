a6f66e74caef3baaa5e28b9f782b7e9cdd6756ed
Michael Mackenzie <vebbed@gmail.com>
2013-10-07 02:03:08 +0000

    no longer need /rental on the end of url

--- application/config/routes.php
+++ application/config/routes.php
@@ -45,7 +45,7 @@ $route['news/create'] = 'news/create';
 $route['news/(:any)'] = 'news/view/$1';
 $route['news'] = 'news';
 $route['(:any)'] = 'pages/view/$1';
-$route['default_controller'] = 'pages/view';
+$route['default_controller'] = 'rental';
 $route['404_override'] = '';
 
 
