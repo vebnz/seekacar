4ec47b864dc3fb9bb46cea986087262c2e7fdf38
Jamie Gracie <k1hosting@gmail.com>
2013-10-08 13:12:43 +1300

    allow same day dropoff

--- js/document.ready.all.js
+++ js/document.ready.all.js
@@ -16,27 +16,27 @@ var DEFAULTAPP = (function ($) {
 			var nowTemp = new Date();
 			var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
 
-			var checkin = $('#pickupdate').datepicker({
+			var pickup = $('#pickupdate').datepicker({
 				format: 'dd/mm/yyyy',
 				onRender: function(date) {
 					return date.valueOf() < now.valueOf() ? 'disabled' : '';
 				}
 			}).on('changeDate', function(ev) {
-				if (ev.date.valueOf() > checkout.date.valueOf()) {
+				if (ev.date.valueOf() > dropoff.date.valueOf()) {
 					var newDate = new Date(ev.date)
 					newDate.setDate(newDate.getDate() + 1);
-					checkout.setValue(newDate);
+					dropoff.setValue(newDate);
 				}
-				checkin.hide();
+				pickup.hide();
 				$('#dropoffdate')[0].focus();
 			}).data('datepicker');
-			var checkout = $('#dropoffdate').datepicker({
+			var dropoff = $('#dropoffdate').datepicker({
 				format: 'dd/mm/yyyy',
 				onRender: function(date) {
-					return date.valueOf() <= checkin.date.valueOf() ? 'disabled' : '';
+					return date.valueOf() < pickup.date.valueOf() ? 'disabled' : '';
 				}
 			}).on('changeDate', function(ev) {
-				checkout.hide();
+				dropoff.hide();
 			}).data('datepicker');
 		});
 
