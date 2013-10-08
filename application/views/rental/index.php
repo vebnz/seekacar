b9c8a1420729f422b852c7b95ec0d753f989f5c6
Jamie Gracie <k1hosting@gmail.com>
2013-10-08 13:05:35 +1300

    put js in the right place

--- application/views/rental/index.php
+++ application/views/rental/index.php
@@ -1,31 +1,3 @@
-<script type='text/javascript' language='javascript'>
-$(document).ready(function() {	
-	var nowTemp = new Date();
-	var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
-
-	var checkin = $('#pickupdate').datepicker({
-		format: 'dd/mm/yyyy',
-		onRender: function(date) {
-			return date.valueOf() < now.valueOf() ? 'disabled' : '';
-		}
-	}).on('changeDate', function(ev) {
-		if (ev.date.valueOf() > checkout.date.valueOf()) {
-			var newDate = new Date(ev.date)
-			newDate.setDate(newDate.getDate() + 1);
-			checkout.setValue(newDate);
-		}
-		checkin.hide();
-		$('#dropoffdate')[0].focus();
-	}).data('datepicker');
-	var checkout = $('#dropoffdate').datepicker({
-		onRender: function(date) {
-			return date.valueOf() <= checkin.date.valueOf() ? 'disabled' : '';
-		}
-	}).on('changeDate', function(ev) {
-		checkout.hide();
-	}).data('datepicker');
-});
-</script>
  		<div class="app_view">
 		<div class="hero">
 			<ul class="slideshow">
--- js/document.ready.all.js
+++ js/document.ready.all.js
@@ -12,18 +12,37 @@ var DEFAULTAPP = (function ($) {
     default_init = function() {
 
         /* Javascript goes in here */
-        $(function() {
-            $("#pickupdate, #dropoffdate").datepicker({
-                dateFormat : 'dd/mm/yy',
-                changeMonth : true,
-                changeYear : true,
-                minDate: 0,
-            });
-        });
+		$(function() {	
+			var nowTemp = new Date();
+			var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
 
-        $(function() {
-            $('#pickuptime, #dropofftime').timepicker({ 'scrollDefaultNow': true });
-        });
+			var checkin = $('#pickupdate').datepicker({
+				format: 'dd/mm/yyyy',
+				onRender: function(date) {
+					return date.valueOf() < now.valueOf() ? 'disabled' : '';
+				}
+			}).on('changeDate', function(ev) {
+				if (ev.date.valueOf() > checkout.date.valueOf()) {
+					var newDate = new Date(ev.date)
+					newDate.setDate(newDate.getDate() + 1);
+					checkout.setValue(newDate);
+				}
+				checkin.hide();
+				$('#dropoffdate')[0].focus();
+			}).data('datepicker');
+			var checkout = $('#dropoffdate').datepicker({
+				format: 'dd/mm/yyyy',
+				onRender: function(date) {
+					return date.valueOf() <= checkin.date.valueOf() ? 'disabled' : '';
+				}
+			}).on('changeDate', function(ev) {
+				checkout.hide();
+			}).data('datepicker');
+		});
+
+        //$(function() {
+        //    $('#pickuptime, #dropofftime').timepicker({ 'scrollDefaultNow': true });
+        //});
 
     }
 
