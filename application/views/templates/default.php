0e83f8940467ecf7dd52623f8b5dd0a20b81791e
Jamie Gracie <k1hosting@gmail.com>
2013-10-10 14:16:11 +1300

    New template design

--- application/views/templates/default.php
+++ application/views/templates/default.php
@@ -10,10 +10,9 @@
     <!-- Le styles -->
     <!--<link href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" rel="stylesheet" />-->
     <link href="/css/bootstrap.css" rel="stylesheet" />
-    <link href="/css/bootstrap-responsive.css" rel="stylesheet" />
     <link href="/css/extra.css" rel="stylesheet" />
     <link href="/css/jquery.timepicker.css" rel="stylesheet" />
-	<link href="/css/datepicker.css" rel="stylesheet" />
+    <link href="/css/datepicker.css" rel="stylesheet" />
     <style type="text/css">
 
     </style>
@@ -25,46 +24,33 @@
       <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.0/jquery.min.js"></script>
   </head>
   <body>
-    <div class="navbar navbar-fixed-top">
-        <div class="navbar-inner">
-            <div class="container-fluid">
-                <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </a>
-                <a class="brand" href="#"><img src="" width="111" height="30" alt="LetsGetRental" /></a>
-                <div class="nav-collapse">
-                    <ul class="nav">
-                        <li class="active"><a href="#">Home</a></li>
-                        <li><a href="#about">About</a></li>
-                        <li><a href="#contact">Contact</a></li>
-                    </ul>
-                </div><!--/.nav-collapse -->
-            </div>
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Seek A Car</a>
         </div>
-    </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div><!--/.navbar-collapse -->
+      </div>
+    </div>    
     <?php echo ! empty($content) ? $content : ""?>
     <!-- Le javascript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="twitter-bootstrap-v2/docs/assets/js/jquery.js"></script>
     <!--<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>-->
-    <script src="twitter-bootstrap-v2/docs/assets/js/bootstrap-transition.js"></script>
-    <script src="twitter-bootstrap-v2/docs/assets/js/bootstrap-alert.js"></script>
-    <script src="twitter-bootstrap-v2/docs/assets/js/bootstrap-modal.js"></script>
-    <script src="twitter-bootstrap-v2/docs/assets/js/bootstrap-dropdown.js"></script>
-    <script src="twitter-bootstrap-v2/docs/assets/js/bootstrap-scrollspy.js"></script>
-    <script src="twitter-bootstrap-v2/docs/assets/js/bootstrap-tab.js"></script>
-    <script src="twitter-bootstrap-v2/docs/assets/js/bootstrap-tooltip.js"></script>
-    <script src="twitter-bootstrap-v2/docs/assets/js/bootstrap-popover.js"></script>
-    <script src="twitter-bootstrap-v2/docs/assets/js/bootstrap-button.js"></script>
-    <script src="twitter-bootstrap-v2/docs/assets/js/bootstrap-collapse.js"></script>
-    <script src="twitter-bootstrap-v2/docs/assets/js/bootstrap-carousel.js"></script>
-    <script src="twitter-bootstrap-v2/docs/assets/js/bootstrap-typeahead.js"></script>
     <script type="text/javascript" src="/js/document.ready.all.js"></script>
     <script type="text/javascript" src="/js/jquery.timepicker.min.js"></script>
-	<script type="text/javascript" src="/js/bootstrap-datepicker.js"></script>
+    <script type="text/javascript" src="/js/bootstrap-datepicker.js"></script>
 
     </body>
 </html>
