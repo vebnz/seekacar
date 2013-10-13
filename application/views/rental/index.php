48c031eeb9d56c4575e9154deed9ccf931bcd79b
Jamie Gracie <k1hosting@gmail.com>
2013-10-14 11:48:47 +1300

    Fixed front page form

--- application/views/rental/index.php
+++ application/views/rental/index.php
@@ -16,6 +16,8 @@
                             <?php endforeach ?>
 						</select>
 					</div>
+				</div>
+				<div class="row">
 					<div class="form-group col-lg-4">
 						<div class="input-group">
 							<span class="input-group-addon"><i class="glyphicon glyphicon-calendar"></i></span>
@@ -37,6 +39,8 @@
                             <?php endforeach ?>
 						</select>
 					</div>
+				</div>
+				<div class="row">
 					<div class="form-group col-lg-4">
 						<div class="input-group">
 							<span class="input-group-addon"><i class="glyphicon glyphicon-calendar"></i></span>
@@ -50,8 +54,10 @@
 						</div>
 					</div>
 				</div>
-				<div class="form-group col-lg-offset-9 col-lg-3">
-					<button type="submit" class="btn btn-primary btn-block">Submit</button>
+				<div class="row">
+					<div class="form-group col-lg-offset-9 col-lg-3">
+						<button type="submit" class="btn btn-primary btn-block">Submit</button>
+					</div>
 				</div>
 			</form>
 	        
