7ed1dad901e3e1ec3594224994afeb1838a68dd6
Michael Mackenzie <vebbed@gmail.com>
2013-10-16 22:34:30 +0000

    Fixed up car images

--- application/views/rental/index.php
+++ application/views/rental/index.php
@@ -68,8 +68,9 @@
     <div class="jumbotron ">
 		<div class="container">
 			<div class="col-lg-12 centered">
-				<img src="/images/ace.jpg" alt="Ace Rental Cars" /><img src="/images/apex.jpg" alt="Apex Rental Cars" /><img src="/images/britz.jpg" alt="Britz Rental Cars" /><img src="/images/budget.jpg" alt="Budget Rental Cars" /><img src="/images/omega.jpg" alt="Omega Rental Cars" /><img src="/images/thrifty.jpg" alt="Thrifty Rental Cars" />
-			</div>
+                                <div class="company col-lg-2"><img src="/images/ace.jpg" alt="Ace Rental Cars" /></div><div class="company col-lg-2"><img src="/images/apex.jpg" alt="Apex Rental Cars" /></div><div class="company col-lg-2"><img src="/images/britz.jpg" alt="Britz Rental Cars" /></div><div class="company col-lg-2"><img src="/images/budget.jpg" alt="Budget Rental Cars" /></div><div class="company col-lg-2"><img src="/images/omega.jpg" alt="Omega Rental Cars" /></div><div class="company col-lg-2"><img src="/images/thrifty.jpg" alt="Thrifty Rental Cars" /></div>
+
+            </div>
 		</div>
     </div>
 
