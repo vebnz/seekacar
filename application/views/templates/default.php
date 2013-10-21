edeb63f037e73c371d5ceabafeb5376b47b18e1e
Jamie Gracie <k1hosting@gmail.com>
2013-10-22 09:14:32 +1300

    Added logo to page #8

--- application/views/templates/default.php
+++ application/views/templates/default.php
@@ -32,7 +32,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Seek A Car</a>
+          <a class="navbar-brand" href="http://seekacar.co.nz"><img src="/images/seekacar-logo.png" /></a>
         </div>
         <div class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
