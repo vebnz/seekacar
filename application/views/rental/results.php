0a61526e831ab845b50e1c0ed80ca82b8c2b009f
Jamie Gracie <k1hosting@gmail.com>
2013-10-22 14:57:50 +1300

    Added sort by price. Issue #13

--- application/views/rental/results.php
+++ application/views/rental/results.php
@@ -53,6 +53,24 @@ $(document).ready(function() {
 			$('#vehicles-list').append('<div class="vehicle"><article class=" "><div class="row clearfix"><div class="vehicle-header clearfix"><span class="vehicle-type">[' + car[0].company + '] ' + car[0].title + '</span></div><div class="vehicle-info"><figure><div class="image"><img src="' + car[0].image + '"></div><figcaption class="clearfix"><div class="details"><h1>' + car[0].type + '</h1><div class="features hidden-item"><div class="wrapper"><ul><li>Size: ' + car[0].size + '</li><li>Luggage: 1 Large Suitcase</li><li>Gearbox: ' + car[0].gearbox + '</li></ul></div></div></div></figcaption></figure><div class="pricing"><div class="single"><div class="wrapper"><div><a target="_blank" href="' + car[0].url + '" class="primary priced btn btn-primary">Select</a><strong class="price">$' + car[0].price + '</strong><span> NZD</span></div></div></div></div></div></div></article></div>');	
 		});
     	});
+	$('#sortByPrice').click(function() {
+                // clear results
+                $('#vehicles-list').empty();
+                // sort the array by company name
+                disArray.sort(function(a, b){
+                        var a1= a[0].price, b1= b[0].price;
+                        if(a1== b1) return 0;
+                                return a1< b1? 1: -1;
+                        });
+                
+                // re-draw the results
+                $.each(disArray, function(i, car)  {
+                        $('#vehicles-list').append('<div class="vehicle"><article class=" "><div class="row clearfix"><div class="vehicle-header clearfix"><span class="vehicle-type">[' + car[0].company + '] ' + car[0].title + '</span></div><div class=
+"vehicle-info"><figure><div class="image"><img src="' + car[0].image + '"></div><figcaption class="clearfix"><div class="details"><h1>' + car[0].type + '</h1><div class="features hidden-item"><div class="wrapper"><ul><li>Size: ' + car[0].size + '</li>
+<li>Luggage: 1 Large Suitcase</li><li>Gearbox: ' + car[0].gearbox + '</li></ul></div></div></div></figcaption></figure><div class="pricing"><div class="single"><div class="wrapper"><div><a target="_blank" href="' + car[0].url + '" class="primary price
+d btn btn-primary">Select</a><strong class="price">$' + car[0].price + '</strong><span> NZD</span></div></div></div></div></div></div></article></div>');      
+                });
+        });
 	$('#companies input[type=checkbox]').change(function() {
     		$('#vehicles-list').empty();
 		disArray = carArray;
