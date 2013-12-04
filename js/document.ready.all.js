0feed19f37f417952ef4ab6093cddf29861246d3
Michael Mackenzie <vebbed@gmail.com>
2013-12-04 22:49:55 +1300

    testing debug hide

--- js/document.ready.all.js
+++ js/document.ready.all.js
@@ -12,6 +12,8 @@ var DEFAULTAPP = (function ($) {
     default_init = function() {
 
         /* Javascript goes in here */
+        $("#debug").hide(); // hide debug stuff at bottom of screen
+
 		$(function() {	
 			var nowTemp = new Date();
 			var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate()+1, 0, 0, 0, 0);
