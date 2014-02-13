f8197a36957241ea66ed6aa57bdc9a29104e40c3
Jamie Gracie <k1hosting@gmail.com>
2014-02-13 13:42:18 +1300

    Fixing git pull

--- application/controllers/git.php
+++ application/controllers/git.php
@@ -6,9 +6,18 @@ class git extends CI_Controller {
                 parent::__construct();
         }
 
-        public function pull() {				
-                echo shell_exec("git pull");
-        }
+        public function pull() {
+			echo "<h2>Running git...</h2>";
+			echo "<pre>";
+			echo shell_exec("git status");
+			echo "</pre>";
+
+			echo "---";
+
+			echo "<pre>";
+			echo shell_exec("git pull");
+			echo "</pre>";
+		}
 }
 
 ?>
