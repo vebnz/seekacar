547a0353527f1af78492402a4b50f6ddc989bd88
Jamie Gracie <k1hosting@gmail.com>
2014-02-13 16:07:12 +1300

    Fixing loading div #24

--- application/views/rental/results.php
+++ application/views/rental/results.php
@@ -233,7 +233,7 @@ $(document).ready(function() {
 			<div id="cars" class="col-lg-8">				
 				<div id="debug">
 					<div id="vehicles-list">
-						<div id="result_table"><p style="text-align: center;"><img src="http://www.sanbaldo.com/wordpress/wp-content/bigrotation2.gif" id="img-load" /> Finding Cars...</p></div>
+						<div id="result_table" style="clear: both; z-index: 999;"><p style="text-align: center;"><img src="http://www.sanbaldo.com/wordpress/wp-content/bigrotation2.gif" id="img-load" /> Finding Cars...</p></div>
 					</div>
 					<table cellpadding="0" cellspacing="0" border="0" class="table table-striped table-bordered" id="carTable">
 						<thead>
