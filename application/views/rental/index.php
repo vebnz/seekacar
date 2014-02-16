f5d8f400df0bfe67324572f1fc7027fd28b6a01a
Jamie Gracie <k1hosting@gmail.com>
2014-02-17 11:59:39 +1300

    Added analytics

--- application/views/rental/index.php
+++ application/views/rental/index.php
@@ -1,3 +1,13 @@
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-48131048-1', 'seekacar.co.nz');
+  ga('send', 'pageview');
+
+</script>
 <div id="header" style="display:block;">
     <!--<div id="background-image"></div>-->
     <ul class="list-unstyled" id="bgimage">
